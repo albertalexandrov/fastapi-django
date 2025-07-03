46a6a02eddb1415c3ce82e4665b9af6c79af23ec
albertalexandrov <albert-ugatu@list.ru>
2025-07-03 21:48:53 +0300

    prometheus

--- fastapi_django/app.py
+++ fastapi_django/app.py
@@ -2,6 +2,7 @@ from functools import partial
 from pathlib import Path
 
 from fastapi import APIRouter, FastAPI
+from prometheus_fastapi_instrumentator import PrometheusFastApiInstrumentator
 from starlette.staticfiles import StaticFiles
 
 from .conf import settings
@@ -11,13 +12,29 @@ APP_ROOT = Path(__file__).parent
 
 
 def include_docs_router(app: FastAPI, router: APIRouter) -> None:
+    print("===> settings.API_DOCS_ENABLED", settings.API_DOCS_ENABLED)
     if settings.API_DOCS_ENABLED:
         app.mount(f"{settings.API_PREFIX}/static", StaticFiles(directory=APP_ROOT / "static"), name="static")
         router.include_router(docs_router)
 
 
+def setup_prometheus(app: FastAPI) -> None:
+    # TODO: проверить, что prometheus-fastapi-instrumentator установлен
+    print("===> settings.PROMETHEUS_ENABLED", settings.PROMETHEUS_ENABLED)
+    if settings.PROMETHEUS_ENABLED:
+        instrumentator = PrometheusFastApiInstrumentator(should_group_status_codes=False)
+        instrumentator = instrumentator.instrument(app)
+        instrumentator.expose(
+            app,
+            should_gzip=settings.PROMETHEUS_SHOULD_GZIP,
+            name=settings.PROMETHEUS_NAME,
+            tags=["Метрики"]
+        )
+        # TODO: остальные настройки в settings
+
+
 def include_routers(app: FastAPI) -> None:
-    router = APIRouter(prefix=settings.API_PREFIX)
+    router = APIRouter()
     include_docs_router(app, router)
     app.include_router(router)
 
@@ -32,5 +49,6 @@ application = FastAPI(
     openapi_url=f"{settings.API_PREFIX}/docs/openapi.json"
 )
 # TODO: настроить урлы
-include_routers(application)
 application.include_router = partial(application.include_router, prefix=settings.API_PREFIX)
+include_routers(application)
+setup_prometheus(application)
--- fastapi_django/conf/global_settings.py
+++ fastapi_django/conf/global_settings.py
@@ -4,5 +4,9 @@ API_DESCRIPTION = "API description not specified"
 API_DOCS_ENABLED = False
 API_VERSION = "API version not specified"
 API_DEBUG = False
-API_PREFIX = None
+API_PREFIX = ""
 MIDDLEWARES = ["default.middleware.example.Middleware"]
+
+PROMETHEUS_ENABLED = False
+PROMETHEUS_SHOULD_GZIP = True
+PROMETHEUS_NAME = "prometheus_metrics"
