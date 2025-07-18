705233f30d91fcd20d575c19d6e72c64904fa07f
albertalexandrov <albert-ugatu@list.ru>
2025-07-18 14:10:35 +0300

    описаны предложения по валидации входящих данных

--- fastapi_django/exceptions/http.py
+++ fastapi_django/exceptions/http.py
@@ -10,3 +10,8 @@ class HTTP401Exception(HTTPException):
 class HTTP403Exception(HTTPException):
     def __init__(self, detail: str = "Forbidden", headers: dict | None = None) -> None:
         super().__init__(status.HTTP_403_FORBIDDEN, detail=detail, headers=headers)
+
+
+class HTTP404Exception(HTTPException):
+    def __init__(self, detail: str = "Not found", headers: dict | None = None) -> None:
+        super().__init__(status.HTTP_404_NOT_FOUND, detail=detail, headers=headers)
